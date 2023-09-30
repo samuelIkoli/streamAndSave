1a3f3d746ce88185cbfd4e617005c74f52266c59
Samuel Ikoli <98848723+samuelIkoli@users.noreply.github.com>
2023-09-30 17:30:14 +0100

    second commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const dotenv = require('dotenv').config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
