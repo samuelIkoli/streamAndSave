81460a3cd22852cac021ab4a1fafccdc7b490f7a
Samuel Ikoli <98848723+samuelIkoli@users.noreply.github.com>
2023-10-02 16:03:49 +0100

    Documentation added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 const dotenv = require('dotenv').config();
 const path = require('path');
 
