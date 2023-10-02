c88e9145bd4c5832bf75508f1bc8722b15c7220e
Samuel Ikoli <98848723+samuelIkoli@users.noreply.github.com>
2023-10-02 16:24:15 +0100

    ec 2 instance

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8686;
+const port = process.env.PORT || 3000;
 const dotenv = require('dotenv').config();
 const path = require('path');
 
